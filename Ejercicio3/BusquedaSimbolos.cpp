b2500c936404a6f5667ac7bd38a53662bdbdb00f
GabriHR <ghernrod@myuax.com>
2023-12-05 11:46:40 +0100

    Funcion main para probar que funciona la clase declarada anteriormente. Utilización de bucles if-else para manejar todos los casos posibles.

--- Ejercicio3/BusquedaSimbolos.cpp
+++ Ejercicio3/BusquedaSimbolos.cpp
@@ -29,4 +29,32 @@ public:
     }
 };
 
+int main() {
+    Environment env;
 
+    env.insert("a", 4);
+    env.insert("b", 16);
+
+    auto resultX = env.lookup("a");
+    if (resultX.first) {
+        cout << "El valor de a es: " << resultX.second << endl;
+    } else {
+        cerr << "Error: El símbolo 'a' no está definido." << endl;
+    }
+
+    auto resultY = env.lookup("y");
+    if (resultY.first) {
+        cout << "El valor de b es: " << resultY.second << endl;
+    } else {
+        cerr << "Error: El valor 'b' no esta definido." << endl;
+    }
+
+    auto resultZ = env.lookup("z");
+    if (resultZ.first) {
+        cout << "El valor de c es: " << resultZ.second << endl;
+    } else {
+        cerr << "Error: El valor 'c' no esta definido." << endl;
+    }
+
+    return 0;
+}
