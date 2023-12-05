064fb588543b677c4a4697c38095149f0c17d40c
GabriHR <ghernrod@myuax.com>
2023-12-05 12:26:27 +0100

    Explicacion ejercicio2

--- Ejercicio2/InsertarSimbolos.cpp
+++ Ejercicio2/InsertarSimbolos.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <map>
 #include <string>
-using namespace std;
-class Environment {
+using namespace std; // std::map, std::string, std::cerr, std::endl
+class Environment { // Environment class
 private:
-    map<string, int> symbolTable;
+    map<string, int> symbolTable; // symbolTable map
 
-public:
+public:  // public methods
 
-    Environment() {
+    Environment() { // Constructor
 
     }
 
-    bool insert(const string& name, int value) {
-        auto result = symbolTable.insert(make_pair(name, value));
-        return result.second;
+    bool insert(const string& name, int value) { // insert method
+        auto result = symbolTable.insert(make_pair(name, value)); // insert the pair
+        return result.second; // return the result
     }
 
     int getSymbolValue(const string& name) {
-        if (symbolTable.find(name) != symbolTable.end()) {
+        if (symbolTable.find(name) != symbolTable.end()) { // if the symbol is in the table
             return symbolTable[name];
-        } else {
-            cerr << "Error: El valor " << name << " no esta definido." <<endl;
+        } else { // if the symbol is not in the table
+            cerr << "Error: El valor " << name << " no esta definido." <<endl; // print the error
             return 0;
         }
     }
@@ -29,28 +29,28 @@ public:
 };
 
 int main() {
-    Environment env;
+    Environment env; // create an environment
 
-    if (env.insert("a", 4)) {
-        cout << "Simbolo 'a' insertado con exito." << endl;
-    } else {
-        cerr << "Error: Simbolo 'a' ya existe en la tabla." << endl;
+    if (env.insert("a", 4)) { // insert the symbol
+        cout << "Simbolo 'a' insertado con exito." << endl; // print the success message
+    } else { // if the symbol is already in the table
+        cerr << "Error: Simbolo 'a' ya existe en la tabla." << endl; // print the error
     }
 
-    if (env.insert("a", 16)) {
-        cout << "Simbolo 'a' insertado con exito." << endl;
-    } else {
-        cerr << "Error: Simbolo 'a' ya existe en la tabla." << endl;
+    if (env.insert("a", 16)) { // insert the symbol
+        cout << "Simbolo 'a' insertado con exito." << endl; // print the success message
+    } else { // if the symbol is already in the table
+        cerr << "Error: Simbolo 'a' ya existe en la tabla." << endl; // print the error
     }
 
-    if (env.insert("b", 22)) {
-        cout << "Simbolo 'b' insertado con exito." << endl;
-    } else {
-        cerr << "Error: Simbolo 'b' ya existe en la tabla." << endl;
+    if (env.insert("b", 22)) { // insert the symbol
+        cout << "Simbolo 'b' insertado con exito." << endl; // print the success message
+    } else { // if the symbol is already in the table
+        cerr << "Error: Simbolo 'b' ya existe en la tabla." << endl; // print the error
     }
 
-    cout << "El valor de a es: " << env.getSymbolValue("a") << endl;
-    cout << "El valor de b es: " << env.getSymbolValue("b") << endl;
+    cout << "El valor de a es: " << env.getSymbolValue("a") << endl; // print the value of the symbol
+    cout << "El valor de b es: " << env.getSymbolValue("b") << endl; // print the value of the symbol
 
     return 0;
 }
