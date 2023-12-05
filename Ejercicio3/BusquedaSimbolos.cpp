bb0ea432bb066d7db5adb990cc91ad721c266034
GabriHR <ghernrod@myuax.com>
2023-12-05 12:28:16 +0100

    Explicacion ejercicio3

--- Ejercicio3/BusquedaSimbolos.cpp
+++ Ejercicio3/BusquedaSimbolos.cpp
@@ -3,56 +3,56 @@
 #include <string>
 #include <utility>
 using namespace std;
-class Environment {
+class Environment { // Tabla de simbolos
 private:
-    map<string, int> symbolTable;
+    map<string, int> symbolTable; // Mapa de simbolos
 
 public:
-    Environment() {
+    Environment() { // Constructor
 
     }
 
 
-    bool insert(const string& name, int value) {
-        auto result = symbolTable.insert(make_pair(name, value));
+    bool insert(const string& name, int value) { // Insertar un simbolo
+        auto result = symbolTable.insert(make_pair(name, value)); // Insertar un par clave-valor
         return result.second;
     }
 
 
-    pair<bool, int> lookup(const string& name) const {
-        auto it = symbolTable.find(name);
-        if (it != symbolTable.end()) {
-            return make_pair(true, it->second);
-        } else {
+    pair<bool, int> lookup(const string& name) const { // Buscar un simbolo
+        auto it = symbolTable.find(name); // Buscar un elemento en el mapa
+        if (it != symbolTable.end()) { // Si el elemento se encuentra en el mapa
+            return make_pair(true, it->second); // Devolver el valor del elemento
+        } else { // Si el elemento no se encuentra en el mapa
             return make_pair(false, 0);
         }
     }
 };
 
-int main() {
-    Environment env;
+int main() { // Programa principal
+    Environment env; // Tabla de simbolos
 
-    env.insert("a", 4);
-    env.insert("b", 16);
+    env.insert("a", 4); // Insertar un simbolo
+    env.insert("b", 16); // Insertar un simbolo
 
-    auto resultX = env.lookup("a");
-    if (resultX.first) {
-        cout << "El valor de a es: " << resultX.second << endl;
-    } else {
-        cerr << "Error: El símbolo 'a' no está definido." << endl;
+    auto resultX = env.lookup("a"); // Buscar un simbolo
+    if (resultX.first) { // Si el simbolo se encuentra en la tabla
+        cout << "El valor de a es: " << resultX.second << endl; // Mostrar el valor del simbolo
+    } else { // Si el simbolo no se encuentra en la tabla
+        cerr << "Error: El símbolo 'a' no está definido." << endl; // Mostrar un mensaje de error
     }
 
-    auto resultY = env.lookup("y");
-    if (resultY.first) {
-        cout << "El valor de b es: " << resultY.second << endl;
-    } else {
-        cerr << "Error: El valor 'b' no esta definido." << endl;
+    auto resultY = env.lookup("y"); // Buscar un simbolo
+    if (resultY.first) { // Si el simbolo se encuentra en la tabla
+        cout << "El valor de b es: " << resultY.second << endl; // Mostrar el valor del simbolo
+    } else { // Si el simbolo no se encuentra en la tabla
+        cerr << "Error: El valor 'b' no esta definido." << endl; // Mostrar un mensaje de error
     }
 
-    auto resultZ = env.lookup("z");
-    if (resultZ.first) {
+    auto resultZ = env.lookup("c"); // Buscar un simbolo
+    if (resultZ.first) { // Si el simbolo se encuentra en la tabla
         cout << "El valor de c es: " << resultZ.second << endl;
-    } else {
+    } else { // Si el simbolo no se encuentra en la tabla
         cerr << "Error: El valor 'c' no esta definido." << endl;
     }
 
