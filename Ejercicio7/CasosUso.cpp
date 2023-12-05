3dde2f553a64edc62673da2e3f8476e615a04cc5
GabriHR <ghernrod@myuax.com>
2023-12-05 12:23:10 +0100

    Función main para probar que funciona la declaracion de la clase anterior.

--- Ejercicio7/CasosUso.cpp
+++ Ejercicio7/CasosUso.cpp
@@ -36,3 +36,23 @@ public:
 
 };
 
+int main() {
+    Environment gameEnvironment;
+
+    gameEnvironment.insertData("gravity", 9.8f);
+    gameEnvironment.insertData("maxPlayers", 4);
+    gameEnvironment.insertData("gameTitle", std::string("Epic Game"));
+
+    gameEnvironment.insertData("godMode", false);
+    gameEnvironment.insertData("playerHealth", 100);
+
+
+    cout << "Gravedad: " << gameEnvironment.getData<float>("gravity") << endl;
+    cout << "Maximo de jugadores: " << gameEnvironment.getData<int>("maxPlayers") << endl;
+    cout << "Titulo del juego: " << gameEnvironment.getData<std::string>("gameTitle") << endl;
+
+    cout << "Modo dios activado: " << (gameEnvironment.getData<bool>("godMode") ? "Si" : "No") << endl;
+    cout << "Salud del jugador: " << gameEnvironment.getData<int>("playerHealth") << endl;
+
+    return 0;
+}
