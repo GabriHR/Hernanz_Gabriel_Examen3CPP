2cf267d6bced98fd9562093e2a556019af895bbb
GabriHR <ghernrod@myuax.com>
2023-12-05 10:53:56 +0100

    Definición de la estructura básica Environment

---
+++ Desarrollo_Código/Estructura_Basica.cpp
@@ -0,0 +1,10 @@
+#include <map>
+#include <string>
+using namespace std;
+class Environment {
+private:
+    map<std::string, Variant> symbolTable; // Tabla de símbolos
+
+public:
+    // Métodos para manipular la tabla de símbolos
+};
